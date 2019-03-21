0270e8db92107a81c4bb100cd27759e56b729d3d
Nataliya R <n.rudenko@gmail.com>
2019-03-21 16:09:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount
-  attr_accessor :name, :balance, :status
-  attr_reader 
+  attr_accessor :balance, :status
+  attr_reader :name
 def initialize(name)
   @name = name
   @balance = 1000
