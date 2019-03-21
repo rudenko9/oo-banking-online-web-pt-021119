20fb0673ed502eb7515467d20040e10913adf867
Nataliya R <n.rudenko@gmail.com>
2019-03-21 18:49:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,14 @@ def valid?
   @sender.valid? && @receiver.valid? ?  true : false 
   end 
   def execute_transaction
-    
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
   end 
 
 end
