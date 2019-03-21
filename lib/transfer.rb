e268188e5c17e8cf0e31935bf415bcc730d9991b
Nataliya R <n.rudenko@gmail.com>
2019-03-21 17:47:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,8 +3,9 @@ attr_accessor  :transfer, :sender, :receiver
 
 def initialize(transfer, receiver, sender)
   @transfer = transfer 
+  @sender = sender
   @receiver = receiver
-  @sender = sender 
+   
 end 
 
 end
