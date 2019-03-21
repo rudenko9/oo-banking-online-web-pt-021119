f002de73286428bb558dd56e48e9a5946509ddb0
Nataliya R <n.rudenko@gmail.com>
2019-03-21 17:47:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
 attr_accessor  :transfer, :sender, :receiver
 
-def initialize(transfer, receiver, sender)
+def initialize(transfer, sender, receiver)
   @transfer = transfer 
   @sender = sender
   @receiver = receiver
