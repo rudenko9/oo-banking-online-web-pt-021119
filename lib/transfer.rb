a1c2398154d40a3ee2043ed6a004ab7faef4867c
Nataliya R <n.rudenko@gmail.com>
2019-03-21 18:57:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,7 @@ def valid?
   end 
   
   def reverse_transfer
-    if @status = "complete"
+    if @status == "complete"
      @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reverssed"
